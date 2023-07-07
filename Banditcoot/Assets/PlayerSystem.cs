1be7266919e28bc55157c58080d1115a904c0e67
Johnniekynock <97814891+Johnniekynock@users.noreply.github.com>
2023-07-07 02:42:03 -0300

    Starting Character Controller

---
+++ Banditcoot/Assets/PlayerSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    private void Update()
+
+    {
+        h.rotation = ;
+    }
+    #region PRIVATE FIELDS
+    private CharacterController m_characterController => GetComponent<CharacterController>();
+
+    private Transform h => transform;
+
+
+
+    #endregion PRIVATE FIELDS
+
+
+
+
+}
