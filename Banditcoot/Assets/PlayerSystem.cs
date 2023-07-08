58b6da23e1774f0d14c0051f9271886162636960
Johnniekynock <97814891+Johnniekynock@users.noreply.github.com>
2023-07-08 00:07:06 -0300

    Character Movement

--- Banditcoot/Assets/PlayerSystem.cs
+++ Banditcoot/Assets/PlayerSystem.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class PlayerSystem : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     private void Start()
     {
-         
+         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     private void Update()
 
     {
-        h.rotation = ;
+        
+
+
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
+
+
+
     }
+
+    
+
     #region PRIVATE FIELDS
     private CharacterController m_characterController => GetComponent<CharacterController>();
 
-    private Transform h => transform;
+    private Transform transform => transform;
+
+    private Rigidbody rb;
+
+    private float speed = 10f;
+    
+
+
+
+
+
 
 
 
