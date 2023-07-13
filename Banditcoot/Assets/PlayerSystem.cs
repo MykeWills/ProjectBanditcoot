afee646ee174254445ae9455ec3fb287dbe55628
Myke Wills <myke_wills@hotmail.com>
2023-07-13 02:07:12 -0300

    Implemented Player movement and jumping + gravity

--- Banditcoot/Assets/PlayerSystem.cs
+++ Banditcoot/Assets/PlayerSystem.cs
@@ -1,54 +1,96 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerSystem : MonoBehaviour
 {
-    
+
+
+    [Serializable]
+    public struct PlayerControl
+    {
+        public float speed;
+        public float gravity;
+        public float jumpForce;
+        public bool isJumping;
+        public bool isGrounded;
+        public bool isMoving;
+        public bool isRunnning;
+        public bool isDashing;
+
+    }
+
+    public PlayerControl playerControl;
+
+
     // Start is called before the first frame update
     private void Start()
     {
-         rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     private void Update()
 
     {
-        
+        MovePlayer();
 
-
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float zMove = Input.GetAxisRaw("Vertical");
-
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
-
-
-
+       
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        playerControl.isJumping = false;
     }
 
-    
+    private void MovePlayer()
+    {
+        float xMove = inputPlayer.GetAxisRaw("Horizontal");
+        float zMove = inputPlayer.GetAxisRaw("Vertical");
+
+        if (!inputPlayer.GetButton("Jump")) jumpTimer++;
+
+        else if (inputPlayer.GetButton("Jump") && jumpTimer >= 1)
+        {
+            moveDirection.y = playerControl.jumpForce;
+            playerControl.isJumping = true;
+            jumpTimer = 0;
+        }
+
+        Debug.Log(moveDirection.y);
+
+        moveDirection.y -= time * playerControl.gravity;
+        moveDirection.x = xMove;
+        moveDirection.z = zMove;
+
+
+        playerControl.isGrounded = (m_characterController.Move(moveDirection * time * playerControl.speed) & CollisionFlags.Below) != 0;
+    }
+    private void Dash()
+    {
+
+    }
+    private void ReduceDashTime()
+    {
+
+    }
 
     #region PRIVATE FIELDS
     private CharacterController m_characterController => GetComponent<CharacterController>();
 
-    private Transform transform => transform;
+    //private Transform transform => transform;
 
-    private Rigidbody rb;
+    //private Rigidbody rb => GetComponent<Rigidbody>();
+    private float time => Time.deltaTime;
 
-    private float speed = 10f;
-    
-
-
-
-
-
-
-
+    private Vector3 moveDirection = Vector3.zero;
+    private Player inputPlayer => ReInput.players.GetPlayer(0);
+    private float jumpTimer = 0;
 
     #endregion PRIVATE FIELDS
 
 
-
+  
 
 }
