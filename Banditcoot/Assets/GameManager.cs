cd70616825914ed7b123b966fa1f0ae6dab4f998
Myke Wills <myke_wills@hotmail.com>
2023-09-01 02:32:08 -0300

    New Game Manager Script

---
+++ Banditcoot/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+
+    // Start is called before the first frame update
+  
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+
+
+
+    private void Awake()
+    {
+        gameManager = this;
+    }
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
